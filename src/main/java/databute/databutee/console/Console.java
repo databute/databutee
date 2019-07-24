8b8e5708a72a32afdbcb38c118862c9090b4f7dd
Gihwan Kim <ghkim3221@gmail.com>
2019-07-24 20:16:27 +0900

    콘솔 입력 구현

---
+++ src/main/java/databute/databutee/console/Console.java
@@ -0,0 +1,46 @@
+package databute.databutee.console;
+
+import org.apache.commons.lang3.StringUtils;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.reader.ParsedLine;
+import org.jline.reader.impl.DefaultParser;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
+
+import java.io.IOException;
+
+public final class Console {
+
+    private static Console instance;
+
+    private final Terminal terminal;
+    private final LineReader reader;
+
+    private Console() throws IOException {
+        this.terminal = TerminalBuilder.terminal();
+        this.reader = LineReaderBuilder.builder()
+                .terminal(terminal)
+                .parser(new DefaultParser())
+                .build();
+    }
+
+    private void start() {
+        while (true) {
+            final String line = reader.readLine(ConsoleConstants.PROMPT).trim();
+            final ParsedLine parsedLine = reader.getParser().parse(line, 0);
+            if (StringUtils.equalsAny(parsedLine.word(), "quit", "exit")) {
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            instance = new Console();
+            instance.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
