fe6353535faf7ab563cdaeccee6537257cbb36fa
Gihwan Kim <ghkim3221@gmail.com>
2019-07-24 19:23:36 +0900

    메인 클래스 생성

---
+++ src/main/java/databute/databutee/Databutee.java
@@ -0,0 +1,12 @@
+package databute.databutee;
+
+import com.google.common.base.MoreObjects;
+
+public class Databutee {
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .toString();
+    }
+}
