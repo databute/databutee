e5ffef119a61ff6f5c890d5926ef00103ddd207f
Gihwan Kim <ghkim3221@gmail.com>
2019-07-24 19:24:33 +0900

    설정 인터페이스 생성 및 메인 클래스에 추가

--- src/main/java/databute/databutee/Databutee.java
+++ src/main/java/databute/databutee/Databutee.java
@@ -2,11 +2,20 @@ package databute.databutee;
 
 import com.google.common.base.MoreObjects;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 public class Databutee {
+
+    private final DatabuteeConfiguration configuration;
+
+    public Databutee(DatabuteeConfiguration configuration) {
+        this.configuration = checkNotNull(configuration, "configuration");
+    }
 
     @Override
     public String toString() {
         return MoreObjects.toStringHelper(this)
+                .add("configuration", configuration)
                 .toString();
     }
 }
---
+++ src/main/java/databute/databutee/DatabuteeConfiguration.java
@@ -0,0 +1,5 @@
+package databute.databutee;
+
+public interface DatabuteeConfiguration {
+
+}
