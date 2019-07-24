29a61634d9c194928f0aa8c731cfdca23f34a537
Gihwan Kim <ghkim3221@gmail.com>
2019-07-24 22:32:17 +0900

    포트 기본 값 2421로 변경

--- src/main/java/databute/databutee/DatabuteeConstants.java
+++ src/main/java/databute/databutee/DatabuteeConstants.java
@@ -2,7 +2,7 @@ package databute.databutee;
 
 public final class DatabuteeConstants {
 
-    public static final int DEFAULT_PORT = 9090;
+    public static final int DEFAULT_PORT = 2421;
 
     private DatabuteeConstants() {
 
