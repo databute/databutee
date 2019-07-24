7b71d4ea686c676bb9543b39d3a96f6f6b90960d
Gihwan Kim <ghkim3221@gmail.com>
2019-07-24 20:17:04 +0900

    `connect` 명령어 추가

--- src/main/java/databute/databutee/console/Console.java
+++ src/main/java/databute/databutee/console/Console.java
@@ -1,5 +1,6 @@
 package databute.databutee.console;
 
+import databute.databutee.DatabuteeConstants;
 import org.apache.commons.lang3.StringUtils;
 import org.jline.reader.LineReader;
 import org.jline.reader.LineReaderBuilder;
@@ -9,6 +10,9 @@ import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public final class Console {
 
@@ -31,6 +35,22 @@ public final class Console {
             final ParsedLine parsedLine = reader.getParser().parse(line, 0);
             if (StringUtils.equalsAny(parsedLine.word(), "quit", "exit")) {
                 break;
+            } else if (StringUtils.equals(parsedLine.word(), "connect")) {
+                final List<InetSocketAddress> addresses = parsedLine.words().subList(1, parsedLine.words().size())
+                        .stream()
+                        .map(address -> {
+                            final String[] hostnameAndPort = address.split(":");
+                            if (hostnameAndPort.length == 1) {
+                                final String hostname = hostnameAndPort[0];
+                                return new InetSocketAddress(hostname, DatabuteeConstants.DEFAULT_PORT);
+                            } else {
+                                final String hostname = hostnameAndPort[0];
+                                final int port = Integer.parseInt(hostnameAndPort[1]);
+                                return new InetSocketAddress(hostname, port);
+                            }
+                        })
+                        .collect(Collectors.toList());
+                terminal.writer().println("addresses = " + addresses);
             }
         }
     }
