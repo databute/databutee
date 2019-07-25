787ce5d27efbcc7a41b7883ee621e1ed0620fccb
Gihwan Kim <ghkim3221@gmail.com>
2019-07-25 20:09:38 +0900

    설정된 서버 주소들 중 한 주소만 임의로 선택 (#11)

--- src/main/java/databute/databutee/Databutee.java
+++ src/main/java/databute/databutee/Databutee.java
@@ -3,14 +3,20 @@ package databute.databutee;
 import com.google.common.base.MoreObjects;
 import com.google.common.collect.Lists;
 import databute.databutee.network.DatabuterSessionConnector;
+import org.apache.commons.lang3.RandomUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.net.ConnectException;
 import java.net.InetSocketAddress;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
 public class Databutee {
+
+    private static final Logger logger = LoggerFactory.getLogger(Databutee.class);
 
     private final DatabuteeConfiguration configuration;
 
@@ -18,15 +24,23 @@ public class Databutee {
         this.configuration = checkNotNull(configuration, "configuration");
     }
 
-    public CompletableFuture<Void> connect() {
-        final List<CompletableFuture<Void>> futures = Lists.newArrayList();
-
-        for (InetSocketAddress address : configuration.addresses()) {
+    public void connect() throws ConnectException {
+        final List<InetSocketAddress> addresses = Lists.newArrayList(configuration.addresses());
+        while (!addresses.isEmpty()) {
+            final int index = RandomUtils.nextInt(0, addresses.size());
+            final InetSocketAddress address = addresses.remove(index);
             final DatabuterSessionConnector connector = new DatabuterSessionConnector(configuration.loopGroup());
-            futures.add(connector.connect(address));
+
+            try {
+                connector.connect(address).join();
+                logger.info("Connected with server at {}", connector.remoteAddress());
+                return;
+            } catch (CompletionException e) {
+                logger.error("Failed to connect to {}.", address);
+            }
         }
 
-        return CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
+        throw new ConnectException();
     }
 
     @Override
