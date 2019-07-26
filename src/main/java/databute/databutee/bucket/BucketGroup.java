f32618dd0764163976cc2e72971614a275a8eb49
Gihwan Kim <ghkim3221@gmail.com>
2019-07-27 01:07:16 +0900

    버킷 조회 콘솔 명령어 추가

--- src/main/java/databute/databutee/bucket/BucketGroup.java
+++ src/main/java/databute/databutee/bucket/BucketGroup.java
@@ -2,16 +2,34 @@ package databute.databutee.bucket;
 
 import com.google.common.collect.Maps;
 
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Spliterator;
+import java.util.function.Consumer;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
-public class BucketGroup {
+public class BucketGroup implements Iterable<Bucket> {
 
     private final Map<String, Bucket> buckets;
 
     public BucketGroup() {
         this.buckets = Maps.newConcurrentMap();
+    }
+
+    @Override
+    public Iterator<Bucket> iterator() {
+        return buckets.values().iterator();
+    }
+
+    @Override
+    public void forEach(Consumer<? super Bucket> action) {
+        buckets.values().forEach(action);
+    }
+
+    @Override
+    public Spliterator<Bucket> spliterator() {
+        return buckets.values().spliterator();
     }
 
     public Bucket find(String id) {
